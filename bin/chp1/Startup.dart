d7a61da47797ad1552e77b70c71d60ab6b9f5c07
panlijun <panlijun@vip.126.com>
2020-07-27 10:27:25 +0800

    startup

---
+++ bin/chp1/Startup.dart
@@ -0,0 +1,5 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//1.The void keyword indicates (as in Java or C#) that the method does not
+// explicitly return an object (indeed print only produces output to the
+// console), but the return types can be left out.
+main() => print('Hello Dart!');
