7d310a169951e153750eeda293e5cd350d11cb75
panlijun <panlijun@vip.126.com>
2020-07-29 22:41:40 +0800

    DateTime

--- bin/chp1/BankAccount.dart
+++ bin/chp1/BankAccount.dart
@@ -4,21 +4,29 @@ class BankAccount {
   // fields
   String owner, number;
   double balance;
+  DateTime dateCreated, dateModified;
 
   // constructor
-  BankAccount(this.owner, this.number, this.balance);
+  BankAccount(this.owner, this.number, this.balance){
+    dateCreated = DateTime.now();
+  }
 
   // methods
-  deposit(double amount) => balance += amount;
-  withdraw(double amount) => balance -= amount;
+  double deposit(double amount) {
+    dateModified = DateTime.now();
+    return balance += amount;
+  }
+  double withdraw(double amount) => balance -= amount;
 
 }
 
-main() {
+void main() {
   var ba = BankAccount('Tom', '62123456786666', 1000.0);
   print('Initial balance:\t\t${ba.balance}\$');
   ba.deposit(250.0);
   print('Balance after deposit:\t${ba.balance}\$');
   ba.withdraw(100.0);
   print('Balance after withdraw:\t${ba.balance}\$');
+
+  print('Bank account created at: ${ba.dateCreated}');
 }
